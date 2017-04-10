1142f926d04e326a8091a407fc7b5f2191f02208
Sergii Bondarenko <broken@firstvector.org>
2017-04-10 18:34:12 +0400

    [CIKit][Tests] Fix installation script

--- tests/cikit.sh
+++ tests/cikit.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export ANSIBLE_ARGS="-vvvv"
+
 CIKIT_PHP_VERSION=$1
 CIKIT_NODEJS_VERSION=$2
 SETUP_SOLR=$3
@@ -8,13 +10,16 @@ SETUP_SOLR=$3
 : ${CIKIT_NODEJS_VERSION:="6"}
 : ${SETUP_SOLR:="false"}
 
-ANSIBLE_ARGS="-vvvv --cikit-php-version=${CIKIT_PHP_VERSION} --cikit-nodejs-version=${CIKIT_NODEJS_VERSION} --setup-solr=${SETUP_SOLR}"
-
 # Change directory to "tests".
 \cd -P -- $(dirname -- $0)
 # Go to root directory of CIKit.
 \cd ..
 
-./cikit repository --project=cikit-test-project
+./cikit repository \
+  --project=cikit-test-project \
+  --cikit-php-version=${CIKIT_PHP_VERSION} \
+  --cikit-nodejs-version=${CIKIT_NODEJS_VERSION} \
+  --setup-solr=${SETUP_SOLR}
+
 \cd cikit-test-project/
 \vagrant up
