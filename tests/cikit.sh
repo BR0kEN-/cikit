cb4280eb1448ae85026ddcef1b0d7d53e4a7d47b
Sergii Bondarenko <broken@firstvector.org>
2017-04-10 18:28:03 +0400

    [CIKit][Tests] Fix project builder

--- tests/cikit.sh
+++ tests/cikit.sh
@@ -10,6 +10,11 @@ SETUP_SOLR=$3
 
 ANSIBLE_ARGS="-vvvv --cikit-php-version=${CIKIT_PHP_VERSION} --cikit-nodejs-version=${CIKIT_NODEJS_VERSION} --setup-solr=${SETUP_SOLR}"
 
-./cikit repository --project=test
-\cd test
+# Change directory to "tests".
+\cd -P -- $(dirname -- $0)
+# Go to root directory of CIKit.
+\cd ..
+
+./cikit repository --project=cikit-test-project
+\cd cikit-test-project/
 \vagrant up
