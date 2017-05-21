65ca3496087bb4fb99a3dd711f162a031027b29f
Sergii Bondarenko <broken@firstvector.org>
2017-05-21 23:35:09 +0300

    [CIKit] Fix Drupal coding standards in PHP files

---
+++ cmf/all/tests/behat/features/bootstrap/FeatureContext.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Feature context.
+ */
+
+// Contexts.
+use Behat\MinkExtension\Context\RawMinkContext;
+
+// Helpers.
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Class FeatureContext.
+ */
+class FeatureContext extends RawMinkContext {
+
+}
---
+++ cmf/drupal/all/docroot/drush/drushrc.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Drush configuration.
+ *
+ * @var array $options
+ */
+
+// Specify a particular multisite.
+// @see http://www.drupalcontrib.org/api/drupal/contributions!drush!examples!example.drushrc.php/7
+$options['l'] = 'profile';
