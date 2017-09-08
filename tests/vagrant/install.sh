a4cb4f0e72e00a69bcf34b8dd1d867aa38848c14
Sergii Bondarenko <broken@firstvector.org>
2017-09-09 03:06:02 +0400

    [Tests][Vagrant] Fix installation for different archs

--- tests/vagrant/install.sh
+++ tests/vagrant/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# @todo Adapt this script for multi OS operability.
-
 OS="$1"
 ARCH="$2"
 DEST="$3"
@@ -10,7 +8,15 @@ SITE="https://releases.hashicorp.com/vagrant"
 SETUP_EXE=$(wget -qO- "${SITE}" | grep -o '>vagrant_.*<' | head -n1 | tr -d '><')
 VERSION=$(awk -F '_' '{print $2}' <<< "${SETUP_EXE}")
 DOWNLOAD_SECTION="${SITE}/${VERSION}"
-SETUP_MSI="${SETUP_EXE}.msi"
+SETUP_MSI="${SETUP_EXE}"
+
+if [ "64" == "${ARCH}" ]; then
+  SETUP_MSI+="_x86_64"
+else
+  SETUP_MSI+="_i686"
+fi
+
+SETUP_MSI+=".msi"
 
 wget -nc -O "${DEST}/${SETUP_MSI}" "${DOWNLOAD_SECTION}/${SETUP_MSI}"
 echo "${SETUP_EXE}|${SETUP_MSI}|${VERSION}|${DOWNLOAD_SECTION}"
