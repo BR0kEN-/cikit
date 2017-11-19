db13c58f16222133268a7007317abc88d6ce8f3e
Sergii Bondarenko <broken@firstvector.org>
2017-11-19 22:07:32 +0200

    #62: Improve tests

--- tests/lib/cikit.sh
+++ tests/lib/cikit.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-export ANSIBLE_VERBOSITY=2
+TEST_PROJECT="cikit_test_project"
+
+########################################################################################################################
+# Functions
+
+locate_path()
+{
+  local DIR="$1"
+
+  if [ -L "${DIR}" ]; then
+    while [ -L "${DIR}" ]; do
+      DIR="$(\cd "$(\dirname -- "$(\readlink -- "${DIR}")")" && \pwd)"
+    done
+  else
+    DIR="$(\cd -P -- "$(\dirname -- "${DIR}")" && \pwd -P)"
+  fi
+
+  \echo "${DIR}"
+}
 
 # @param int $1
 #   The expected exit code.
@@ -8,7 +26,7 @@ export ANSIBLE_VERBOSITY=2
 #   The arguments fot the "cikit" command.
 # @param string $3
 #   The expected output of the command.
-__check()
+__cikit_test()
 {
   local COMMAND="$2"
   local EXPECTED_OUTPUT="$3"
@@ -22,31 +40,61 @@ __check()
   EXIT_CODE="$?"
 
   if [ "${EXPECTED_EXIT_CODE}" != "${EXIT_CODE}" ]; then
-    echo "The expected exit code is \"${EXPECTED_EXIT_CODE}\" when actual is \"${EXIT_CODE}\"."
+    echo -e "\nThe expected exit code is \"${EXPECTED_EXIT_CODE}\" when actual is \"${EXIT_CODE}\".\n"
   fi
 
   if [[ -n "${EXPECTED_OUTPUT}" && ! "${OUTPUT}" = *"${EXPECTED_OUTPUT}"* ]]; then
-    echo "The expected output of the \"${COMMAND}\" is:"
-    echo "  ${EXPECTED_OUTPUT}"
-    echo "when the actual is:"
-    echo "  ${OUTPUT}"
+    echo -e "\nThe expected output is:\n  ${EXPECTED_OUTPUT}\nwhen actual is:\n  ${OUTPUT}\n"
   fi
 }
 
+########################################################################################################################
+# Set up
+
+cd "$(locate_path "$0")/../../"
+
+SELF_DIR="$(pwd)"
+
+if [ ! -d "${TEST_PROJECT}" ]; then
+  cikit init --project="${TEST_PROJECT}"
+fi
+
+cd "${TEST_PROJECT}"
+
+export ANSIBLE_VERBOSITY=2
+
+########################################################################################################################
+# cikit init
+
 for ARGSET in "" "--project" "--project=1"; do
-  __check \
+  __cikit_test \
     1 \
     "cikit init --dry-run ${ARGSET}" \
     "ERROR: The \"--project\" option is required for the \"init\" command and currently missing or has a value less than 2 symbols."
 done
 
-__check 0 "cikit init --dry-run --project=test" "12"
+__cikit_test \
+  0 \
+  "cikit init --dry-run --project=test" \
+  "$(cat <<-HERE
+ansible-playbook '${SELF_DIR}/scripts/init.yml' -i '${HOME}/.cikit-inventory' -e '{"project": "test"}' -e __targetdir__='${SELF_DIR}/${TEST_PROJECT}'
+HERE
+)"
+
+########################################################################################################################
+# cikit provision.
 
 for ARGSET in "" "--limit" "--limit=1"; do
-  __check \
+  __cikit_test \
     1 \
     "cikit provision --dry-run ${ARGSET}" \
     "ERROR: The \"--limit\" option is required for the \"provision\" command and currently missing or has a value less than 2 symbols."
 done
 
-__check 0 "cikit provision --dry-run --limit=test"
+__cikit_test \
+  0 \
+  "cikit provision --dry-run --limit=test" \
+  "$(cat <<-HERE
+ansible-playbook '${SELF_DIR}/scripts/provision.yml' -i '${HOME}/.cikit-inventory' -l 'test' -e '{"limit": "test"}' -e __targetdir__='${SELF_DIR}/${TEST_PROJECT}'
+HERE
+)"
