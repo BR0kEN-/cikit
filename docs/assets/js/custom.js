320c71ef29e4a002abd3d5b305f51e1d19a45404
Sergii Bondarenko <broken@firstvector.org>
2018-01-20 00:23:32 +0200

    [Docs] Remove jQuery usage from a custom script

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -1,12 +1,14 @@
-(function ($, window) {
+(function (window, document) {
   'use strict';
 
   // Do not allow pinned site on iOS to open links in a browser.
   if (/iphone|ipod|ipad/i.test(window.navigator.userAgent)) {
-    $('a[href]').on('click', function (event) {
-      event.preventDefault();
+    document.querySelectorAll('a[href]').forEach(function (link) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
 
-      location.href = this.href;
+        location.href = this.href;
+      });
     });
   }
-})(window.jQuery, window);
+})(window, window.document);
