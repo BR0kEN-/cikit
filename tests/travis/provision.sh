7df48deea05dd6e5a461c378ec69522f994f3a61
Sergii Bondarenko <broken@firstvector.org>
2018-01-26 09:41:00 +0200

    #49: Oops, correct the name of variable

--- tests/travis/provision.sh
+++ tests/travis/provision.sh
@@ -5,4 +5,4 @@ PROJECT_NAME="test_project"
 cikit init --project="$PROJECT_NAME"
 cd "$PROJECT_NAME"
 
-ANSIBLE_INVENTORY="localhost," EXTRA_ARGS="--web-server=nginx --php-version=7.1 --nodejs-version=6 --ruby-version=2.4.0 --solr-version=6.6.2 --mssql-install" cikit provision --limit=localhost
+ANSIBLE_INVENTORY="localhost," EXTRA_VARS="--web-server=nginx --php-version=7.1 --nodejs-version=6 --ruby-version=2.4.0 --solr-version=6.6.2 --mssql-install" cikit provision --limit=localhost
