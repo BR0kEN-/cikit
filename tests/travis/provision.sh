37df4dcd4d6d51d9d9c117135dff088512bdd9f9
Sergii Bondarenko <broken@firstvector.org>
2018-01-26 09:47:36 +0200

    #49: Try to eliminate host key check

--- tests/travis/provision.sh
+++ tests/travis/provision.sh
@@ -5,4 +5,8 @@ PROJECT_NAME="test_project"
 cikit init --project="$PROJECT_NAME"
 cd "$PROJECT_NAME"
 
-ANSIBLE_INVENTORY="localhost," EXTRA_VARS="--web-server=nginx --php-version=7.1 --nodejs-version=6 --ruby-version=2.4.0 --solr-version=6.6.2 --mssql-install" cikit provision --limit=localhost
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_INVENTORY="localhost,"
+export EXTRA_VARS="--web-server=nginx --php-version=7.1 --nodejs-version=6 --ruby-version=2.4.0 --solr-version=6.6.2 --mssql-install"
+
+cikit provision --limit=localhost
