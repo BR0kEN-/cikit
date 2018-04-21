633a571d872ba360bcc6f2ace3bc1df5c4eefef9
Sergii Bondarenko <broken@firstvector.org>
2018-04-22 03:27:10 +0400

    \#124: [skip bash/init.sh][skip bash/cikit.sh] Try running "cikit reinstall" tests

--- tests/travis/bash/provision.sh
+++ tests/travis/bash/provision.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-PROJECT_NAME="test_project"
-
-cikit init --project="$PROJECT_NAME" --without-sources
-cd "$PROJECT_NAME"
-
+export PROJECT_NAME="test_project"
 export ANSIBLE_HOST_KEY_CHECKING=False
 export EXTRA_VARS="--web-server=nginx --php-version=7.1 --nodejs-version=6 --ruby-version=2.4.0 --solr-version=6.6.3 --mssql-install"
 
+cikit init --project="$PROJECT_NAME" --cmf=drupal
+cd "$PROJECT_NAME"
+
 cikit env/start
 cikit provision
+
+# Test (re-)installation of Drupal
+# @todo Think about WordPress and other CMFs.
+cikit ssh "cikit reinstall"
