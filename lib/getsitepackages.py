a65a1f9a25d1941e3c1395dea5a241766f9c4250
Sergii Bondarenko <broken@firstvector.org>
2018-07-05 13:48:20 +0100

    #153: Use "distutils.sysconfig.get_python_lib()" when "site.getsitepackages()" is unavailable

---
+++ lib/getsitepackages.py
@@ -0,0 +1,12 @@
+import site
+
+if hasattr(site, 'getsitepackages'):
+    print site.getsitepackages()
+else:
+    # If "site" module has no "getsitepackages" function then most probably Python is running inside of "virtualenv".
+    # - https://github.com/pypa/virtualenv/issues/355
+    # - https://github.com/SFTtech/openage/pull/1031
+    # - https://github.com/dmlc/tensorboard/issues/38#issuecomment-343017735
+    from distutils.sysconfig import get_python_lib
+
+    print [get_python_lib()]
