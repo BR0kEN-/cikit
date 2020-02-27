b91283f83e51c86e4567fcfbdc3e38854c0e0df8
Sergii Bondarenko <broken@firstvector.org>
2020-02-27 12:04:56 +0200

    #166: Add PHP 7.3 and 7.4

--- tests/travis/bash/provision.sh
+++ tests/travis/bash/provision.sh
@@ -4,7 +4,7 @@ set -e
 
 export PROJECT_NAME="test_project"
 export ANSIBLE_HOST_KEY_CHECKING=False
-export EXTRA_VARS="--web-server=nginx --php-version=7.1 --nodejs-version=6 --ruby-version=2.4.0 --solr-version=7.7.2 --mssql-install --psql-install --mongodb-install=no"
+export EXTRA_VARS="--web-server=nginx --php-version=7.3 --nodejs-version=6 --ruby-version=2.4.0 --solr-version=7.7.2 --mssql-install --psql-install --mongodb-install=no"
 
 cikit init --project="$PROJECT_NAME" --cmf=drupal
 cd "$PROJECT_NAME"
